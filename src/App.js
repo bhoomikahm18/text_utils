34f6770c27dd77198f3fa0ee75893ea8a1cf434a
Bhoomika <bhoomikahm18@gmail.com>
2023-11-12 14:39:00 +0530

    Adding color mode

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
@@ -5,9 +6,10 @@ import TextForm from './components/TextForm';
 
 let name = 'Bhoomi'
 function App() {
+  const [mode, setMode] = useState('light');
   return (
     <>
-      <Navbar title="Text Utils" about="About Text" />
+      <Navbar title="Text Utils" about="About Text" mode={mode}/>
       <div className='container my-3'>
         <TextForm heading="Enter the text to analize"/>
       </div>
