252a25a8fa41fc437eebb7fd96a782624bf61e50
Bhoomika <bhoomikahm18@gmail.com>
2023-11-12 14:49:27 +0530

    Adding Toggle mode function

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import TextForm from './components/TextForm';
 let name = 'Bhoomi'
 function App() {
   const [mode, setMode] = useState('light');
+
+  function toggleMode() {
+    (mode === 'light') ? (setMode('dark')) : (setMode('light'));
+  }
   return (
     <>
-      <Navbar title="Text Utils" about="About Text" mode={mode}/>
+      <Navbar title="Text Utils" about="About Text" mode={mode} toggleMode={toggleMode} />
       <div className='container my-3'>
-        <TextForm heading="Enter the text to analize"/>
+        <TextForm heading="Enter the text to analize" />
       </div>
       <About />
-      
+
     </>
   );
 }
