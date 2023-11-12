f9a4cdfb3949a284440101d974c674d8f1f84649
Bhoomika <bhoomikahm18@gmail.com>
2023-11-12 12:50:29 +0530

    Adding about component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -7,9 +8,10 @@ function App() {
   return (
     <>
       <Navbar title="Text Utils" about="About Text" />
-      <div className='container my-3'>
+      {/* <div className='container my-3'>
         <TextForm heading="Enter the text to analize"/>
-      </div>
+      </div> */}
+      <About />
       
     </>
   );
