64073def019d6dd9faa170f137665c198069a14e
Bhoomika <bhoomikahm18@gmail.com>
2023-11-12 12:33:21 +0530

    Adding button to convert lower case the text

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -3,23 +3,37 @@ import React, { useState } from 'react'
 function TextForm(props) {
     const [text, setText] = useState("Enter Text Here");
 
-    function handleUpClick(){
+    function handleUpClick() {
         let newText = text.toUpperCase();
         setText(newText);
     }
 
-    function handleOnChange(event){
+    function handleOnChange(event) {
         setText(event.target.value);
+    }
+
+    function handleLowClick() {
+        let newText = text.toLowerCase();
+        setText(newText);
     }
 
     return (
         <>
-            <div className='mb-3'>
-                <h1>{props.heading}</h1>
-                <textarea value={text} onChange={handleOnChange}
-                className='form-control' id="myBox" rows="3"></textarea>
+            <div className='container'>
+                <div className='mb-3'>
+                    <h1>{props.heading}</h1>
+                    <textarea value={text} onChange={handleOnChange}
+                        className='form-control' id="myBox" rows="3"></textarea>
+                </div>
+                <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className='btn btn-primary' onClick={handleLowClick}>Convert to Lowercase</button>
             </div>
-            <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+            <div className='conatiner my-3'>
+                <h1>Your Text Summary</h1>
+                <p>{text.split(" ").length} words & {text.length} characters</p>
+                <p>{0.7 * text.split(" ").length} seconds to read</p>
+                <h2>Preview</h2>
+            </div>
         </>
     )
 }
