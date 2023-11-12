c06a5817cb2e5331cca58e206a83a83d04059fc3
Bhoomika <bhoomikahm18@gmail.com>
2023-11-12 14:57:08 +0530

    Adding dark mode

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ function App() {
   const [mode, setMode] = useState('light');
 
   function toggleMode() {
-    (mode === 'light') ? (setMode('dark')) : (setMode('light'));
+    if(mode === 'light') { 
+      (setMode('dark'));
+      document.body.style.backgroundColor = '#495057' 
+    }else{
+      (setMode('light'));
+      document.body.style.backgroundColor = '#ffffff' 
+
+  }
   }
   return (
     <>
       <Navbar title="Text Utils" about="About Text" mode={mode} toggleMode={toggleMode} />
       <div className='container my-3'>
-        <TextForm heading="Enter the text to analize" />
+        <TextForm heading="Enter the text to analize" mode={mode}/>
       </div>
       <About />
 
