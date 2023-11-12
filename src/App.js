8d8e2c805af478a733212886c399b5be5ffe59a4
Bhoomika <bhoomikahm18@gmail.com>
2023-11-12 11:23:47 +0530

    Setting up emmet and adding lorem to paragraph

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 
+let name = 'Bhoomi'
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div>
+        <h1>Hello {name}</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum facilis facere maiores distinctio quibusdam officia officiis praesentium magni nostrum, quas, magnam exercitationem quidem ipsum, nulla provident aperiam atque. Cupiditate, beatae?</p>
+      </div>
+    </>
   );
 }
 
